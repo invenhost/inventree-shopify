ffe50c3f3ca837b08692084ad263bb56f0e45235
Matthias Mair <matthias.mair@oewf.org>
2021-10-16 20:47:49 +0200

    added admin page

---
+++ admin.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+
+from .models import Product, Variant, InventoryLevel
+
+
+class InventoryLevelAdmin(ImportExportModelAdmin):
+
+    list_display = ('location_id', 'variant', 'available',)
+    list_filter = ('location_id', )
+
+
+admin.site.register(Product, ImportExportModelAdmin)
+admin.site.register(Variant, ImportExportModelAdmin)
+admin.site.register(InventoryLevel, InventoryLevelAdmin)
